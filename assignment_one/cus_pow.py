0d6587e0cda30bfc5d07b29280fdb8d6b9d56321
sharavana20 <sharavana.kumar@multicorewareinc.com>
2025-01-21 18:10:27 +0000

    added the exponent tensor part for 1 dimension

--- assignment_one/cus_pow.py
+++ assignment_one/cus_pow.py
@@ -4,21 +4,65 @@ import torch
 def torch_pow(base, exp):
     shape = base.shape
     res = torch.ones(shape, dtype=base.dtype)
-    # For exponent scalar
-    if not exp.ndim:
-      while exp != 0:
-          if exp % 2 != 0:
-              res = torch.mul(res, base)
-          exp = torch.div(exp, 2, rounding_mode="floor")
-          base = torch.mul(base, base)
+    while exp != 0:
+        if exp % 2 != 0:
+            res = torch.mul(res, base)
+        exp = torch.div(exp, 2, rounding_mode="floor")
+        base = torch.mul(base, base)
     return res
 
 
 def torch_cus_pow(base, exp):
-    if exp < 0:
-        int_datatypes=set(torch.int32,torch.int64,torch.uint8,torch.int8,torch.int16,torch.short,torch.long)
-        exp=torch.tensor(exp,dtype=torch.float)
-        if  base.dtype==torch.int64 or base.dtype==torch.int32:
-            return torch.div(1, torch_pow(base, -exp), rounding_mode="trunc")
-        return torch.div(1, torch_pow(base, -exp))
-    return torch_pow(base, exp)
+    if not torch.is_tensor(exp):
+        exp = torch.tensor(exp, dtype=torch.float)
+    # For exponent scalar
+    if not exp.ndim:
+        if exp < 0:
+            int_datatypes = set(
+                [
+                    torch.int32,
+                    torch.int64,
+                    torch.uint8,
+                    torch.int8,
+                    torch.int16,
+                    torch.short,
+                    torch.long,
+                ]
+            )
+            if base.dtype == torch.int64 or base.dtype == torch.int32:
+                if exp.dtype in int_datatypes:
+                    return torch.div(1, torch_pow(base, -exp), rounding_mode="trunc")
+                return torch.div(1, torch_pow(base, -exp))
+            return torch.div(1, torch_pow(base, -exp))
+        return torch_pow(base, exp)
+    else:
+        return expo_tensor(base,exp)
+
+def expo_tensor(base,exp):
+    b1 = base.reshape(-1)
+    d1 = base.shape
+    d2 = exp.shape
+    m, n = len(d1), len(d2)
+    dims = [1] * max(m, n)
+    d1 = list(d1)
+    d2 = list(d2)
+    if len(d1) < len(d2):
+        d1 = [1] * (n - m) + d1
+        for i in range(max(m, n)):
+            dims[i] = max(d1[i], d2[i])
+        exp_tp = base.reshape(dims)
+        b1 = base.reshape(-1)
+        e1 = exp_tp.reshape(-1)
+    else:
+        d2 = [1] * (m - n) + d2
+        for i in range(max(m, n)):
+            dims[i] = max(d1[i], d2[i])
+        exp_tp = exp.reshape(dims)
+        b1 = base.reshape(-1)
+        e1 = exp_tp.reshape(-1)
+
+    res = torch.ones(b1.shape, dtype=base.dtype)
+
+    for i in range(e1.shape[0]):
+        res[i] = torch_cus_pow(b1[i], e1[i])
+    return res
--- tests/test_pow.py
+++ tests/test_pow.py
@@ -1,18 +1,28 @@
-import pytest, torch
+import pytest, torch, os
 import sys
 
-sys.path.append("/home/shar/pytorch_assignments")
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from assignment_one.cus_pow import torch_cus_pow
 
 test_suite = []
-for i in range(5):
-    base = torch.randn(1, 4)
+for i in range(15):
+    base = torch.randn(1, 5)
+    exp = 0
     if i % 2 == 0:
-        base, exp = torch.tensor([8, 9, 8]), torch.tensor((i + 1))
+        base, exp = torch.tensor([8, 9, 8]), torch.tensor(-(i + 1))
     test_suite.append([base, exp, torch.pow(base, exp)])
+test_suite.append(
+    [torch.tensor([0, 0, 0, 0]), 2, torch.pow(torch.tensor([0, 0, 0, 0]), 2)]
+)
+
+# For exponent tensor when they have the dimension is 1
+
+base = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+exp = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+test_suite.append([base, exp, torch.pow(base, exp)])
 
 
 @pytest.mark.parametrize("base,exp,expected", test_suite)
-def test_Pow(base, exp, expected):
-    assert torch.equal(torch_cus_pow(base,exp),expected)
-
+def test_pow(base, exp, expected):
+    assert torch.equal(torch_cus_pow(base, exp), expected)
